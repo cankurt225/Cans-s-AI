421e3895e6df43befb4bf165794dabf34c30f80b
Can Ahmet Kurt <105139310+cankurt225@users.noreply.github.com>
2025-12-14 10:11:35 +0300

    Delete Server directory

--- Server/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Message
-
-@admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'is_active', 'created_at', 'updated_at']
-    list_filter = ['is_active', 'created_at']
-    search_fields = ['title', 'content']
-    readonly_fields = ['created_at', 'updated_at']
